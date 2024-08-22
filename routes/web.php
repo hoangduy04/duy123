36175948e397fc4e61c731435cf9e49964eb24c1
hoangduy04 <nguyenhoangduy08102004@gmail.com>
2024-08-22 09:12:12 +0700

    hoangduy/baitapMVC

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Events\UserRegisterd;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Jobs\SendMail;
+use App\Models\Post;
+use App\Models\User;
+use App\Mail\PostPublished;
+use App\DataTables\UsersDataTable;
+//use App\Models\Customer;
+use App\Models\People;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    
+    $people = People::all();
+
+    return view('ashop', ['people' => $people]); 
+});
